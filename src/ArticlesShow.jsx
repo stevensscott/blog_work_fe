3cfa317501cce8fcfe916c663d750fe095e9d47f
“Scott <stevensscott29@gmail.com>
2023-07-18 17:48:28 -0400

    updating code

--- src/ArticlesShow.jsx
+++ src/ArticlesShow.jsx
@@ -1,34 +1,37 @@
 export function ArticlesShow(props) {
-
   const handleSubmit = (event) => {
-        event.preventDefault();
-        const params = new FormData(event.target);
-        // props.onUpdateArticle(props.article.id, params, () => event.target.reset());
-        props.onUpdateArticle(props.article.id, { title: params.get("title"), body: params.get("body"), status: params.get("status") }, () => event.target.reset());
-      };
+    event.preventDefault();
+    const params = new FormData(event.target);
+    // props.onUpdateArticle(props.article.id, params, () => event.target.reset());
+    props.onUpdateArticle(
+      props.article.id,
+      { title: params.get("title"), body: params.get("body"), status: params.get("status") },
+      () => event.target.reset()
+    );
+  };
 
-      const handleClick = () => {
-           props.onDestroyArticle(props.article);
-         };
+  const handleClick = () => {
+    props.onDestroyArticle(props.article);
+  };
+
   return (
     <div>
-      <h1>Article Info</h1>     
+      <h1>Article Info</h1>
       <p>Title: {props.article.title}</p>
       <p>Body: {props.article.body}</p>
-      <p>Status: {props.article.status}</p>      
-      <form  onSubmit ={handleSubmit}>
-       <div>
+      <p>Status: {props.article.status}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
           Title: <input defaultValue={props.article.title} name="title" type="text" />
-         </div>
+        </div>
         <div>
           Body: <input defaultValue={props.article.body} name="body" type="text" />
-       </div>
-       <div>
+        </div>
+        <div>
           Status: <input defaultValue={props.article.status} name="status" type="text" />
         </div>
-        
+
         <button type="submit">Update Article</button>
-       
       </form>
       <button onClick={handleClick}>Delete Article</button>
     </div>
--- src/Content.jsx
+++ src/Content.jsx
@@ -2,15 +2,13 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { ArticlesIndex } from "./ArticlesIndex";
 import { ArticlesNew } from "./ArticlesNew";
-import {Modal} from "./Modal";
+import { Modal } from "./Modal";
 import { ArticlesShow } from "./ArticlesShow";
-
 
 export function Content() {
   const [articles, setArticles] = useState([]);
   const [isArticlesShowVisible, setIsArticlesShowVisible] = useState(false);
   const [currentArticle, setCurrentArticle] = useState({});
-
 
   const handleIndexArticles = () => {
     console.log("handleIndexArticles");
@@ -28,7 +26,6 @@ export function Content() {
     });
   };
 
-
   const handleShowArticle = (article) => {
     console.log("handleShowArticle", article);
     setIsArticlesShowVisible(true);
@@ -36,35 +33,34 @@ export function Content() {
   };
 
   const handleUpdateArticle = (id, params, successCallback) => {
-        console.log("handleUpdateArticle", params);
-        axios.patch(`http://localhost:3000/articles/${id}.json`,  { article: params } ).then((response) => {
-          setArticles(
-            articles.map((article) => {
-              if (article.id === response.data.id) {
-                return response.data;
-             } else {
-               return article;
-             }
-           })
-         );
-         successCallback();
-          handleClose();
-         });
-       };
-    
+    console.log("handleUpdateArticle", params);
+    axios.patch(`http://localhost:3000/articles/${id}.json`, { article: params }).then((response) => {
+      setArticles(
+        articles.map((article) => {
+          if (article.id === response.data.id) {
+            return response.data;
+          } else {
+            return article;
+          }
+        })
+      );
+      successCallback();
+      handleClose();
+    });
+  };
+
   const handleClose = () => {
     console.log("handleClose");
     setIsArticlesShowVisible(false);
   };
 
-
-     const handleDestroyArticle = (article) => {
-      console.log("handleDestroyArticle", article);
-         axios.delete(`http://localhost:3000/articles/${article.id}.json`).then((response) => {
-          setArticles(articles.filter((a) => a.id !== article.id));
-        handleClose();
-       });
-      };
+  const handleDestroyArticle = (article) => {
+    console.log("handleDestroyArticle", article);
+    axios.delete(`http://localhost:3000/articles/${article.id}.json`).then((response) => {
+      setArticles(articles.filter((a) => a.id !== article.id));
+      handleClose();
+    });
+  };
 
   useEffect(handleIndexArticles, []);
 
@@ -72,10 +68,12 @@ export function Content() {
     <div>
       <ArticlesNew onCreateArticle={handleCreateArticle} />
       <ArticlesIndex articles={articles} onShowArticle={handleShowArticle} />
-      <Modal show ={isArticlesShowVisible} onClose={handleClose}>
-
-        <ArticlesShow article={currentArticle} onUpdateArticle={handleUpdateArticle} onDestroyArticle={handleDestroyArticle}/>
-
+      <Modal show={isArticlesShowVisible} onClose={handleClose}>
+        <ArticlesShow
+          article={currentArticle}
+          onUpdateArticle={handleUpdateArticle}
+          onDestroyArticle={handleDestroyArticle}
+        />
       </Modal>
     </div>
   );
